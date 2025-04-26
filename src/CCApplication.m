36de681fb6a847a4c17ae247f2741e4187ab81e9
Christian Mayer <christian@fox21.at>
2025-04-26 13:21:09 +0200

    Format files.

--- src/CCApplication.m
+++ src/CCApplication.m
@@ -8,14 +8,14 @@
 @implementation CCApplication
 
 - (void)sendEvent:(NSEvent*)anEvent{
-	
+
 	if([anEvent type] == NSSystemDefined && [anEvent subtype] == 8){
 		int data = [anEvent data1];
 		int keyCode = ((data & 0xffff0000) >> 16);
 		int keyFlags = (data & 0x0000ffff);
 		int keyState = (((keyFlags & 0xff00) >> 8)) == 0xa;
 		BOOL keyIsRepeat = (keyFlags & 0x1) > 0;
-		
+
 		if(keyState){
 			int cr;
 			switch(keyCode){
@@ -23,13 +23,13 @@
 					cr = system("cmus-remote -u");
 					NSLog(@"%d %d %d %d  Play", keyCode, keyState, keyIsRepeat, cr);
 					break;
-				
+
 				case NX_KEYTYPE_FAST:
 				case NX_KEYTYPE_NEXT:
 					cr = system("cmus-remote -n");
 					NSLog(@"%d %d %d %d  Next", keyCode, keyState, keyIsRepeat, cr);
 					break;
-				
+
 				case NX_KEYTYPE_REWIND:
 				case NX_KEYTYPE_PREVIOUS:
 					cr = system("cmus-remote -r");
@@ -38,7 +38,7 @@
 			}
 		}
 	}
-	
+
 	[super sendEvent:anEvent];
 }
 
